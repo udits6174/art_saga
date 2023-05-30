847a70530cbb07f6b14bdc488f7beefed57cd97c
udits6174 <udits502@gmail.com>
2023-05-30 14:14:18 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+const Home = () => (
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex" />
+    <h1>Lunn</h1>
+  </main>
+);
+export default Home;
